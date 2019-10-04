9ce14587096f56bcf40d7e8d7a2c3273f4a616fc
poch-umd <poch@terpmail.umd.edu>
2019-10-04 13:10:19 -0400

    initial def shell wd

--- week/4/writeup/shell.rb
+++ week/4/writeup/shell.rb
@@ -31,18 +31,31 @@ class CdDir
     @path = path_to_set
   end
 
+  def path_prompt
+    @path_components.join( '/' )
+  end
 end
 
 menu = "Please enter a number:\n1. shell\n2. pull <remote-path> <local-path>\n3. help\n4. quit"
-wd = CdDir.new '/'
 
 def shell wd
-  require 'socket'
   # print a shell prompt w/ the directory
   # allow the user to enter a command
   # if it's a cd command then change the CdDir
   # if it's a command then output the ; cd ; <command>
   # capture the output and show it on the screen for the user
+  prompt = wd.path_prompt + '> '
+  print prompt
+  while input = gets.chomp do
+    case input
+      when /^\s*quit\s*$/i
+        break
+      when /^\s*cd\s*(.+)\s*$/
+        puts selection
+    end
+    require 'socket'
+    print prompt
+  end
 end
 
 def pull remote, local
@@ -50,13 +63,15 @@ def pull remote, local
   # capture output to the local file
 end
 
+wd = CdDir.new '/'
+
 puts menu
 while selection = gets.chomp do
   case selection
     when /^\s*quit\s*$/i
       break
     when /^\s*shell\s*$/i
-      puts selection
+      shell wd
     when /^\s*pull (.*?) (.*?)\s*$/i
       puts selection
     when /^\s*help\s*$/i
